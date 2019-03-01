a1977be8fadb3403660d2c0bfd01cc83b74b1ef8
epf.samuel.susoliak <samuel.susoliak@gmail.com>
2019-03-01 16:20:23 +0100

    algorithm now running

--- venv/script.py
+++ venv/script.py
@@ -38,58 +38,76 @@ for string in sorted_dictionary_array:
 #kontrolny zapis do suboru, pouzity na overenia spravneho generovanie p a q hodnot
 file = open('sorted_dict', 'w')
 file.write(json.dumps(sorted_dictionary))
+file.close()
 
 #generovanie poli P a Q pre algoritmus
 P = []
 Q = []
-last_occ_index = 0
+
+#pomocne pole potrebne pre pocitanie parcialnych sum
+sorted_dictionary_values = list(sorted_dictionary.values())
+
+#funkcia pre vypocet q
+def q_value_generator (bef_occ_index,now_index):
+    cumulative_sum = 0
+    if now_index == 0:
+        return 0
+    else:
+        for i in range(bef_occ_index+1,now_index-1):
+            cumulative_sum += sorted_dictionary_values[i]
+        return cumulative_sum/sum_of_all_occurrences
+
+before_occ_index = 0
 index=0
 for key, value in sorted_dictionary.items():
     if key in over_limit_words_array:
         P.append(value/sum_of_all_occurrences)
-        indices = [last_occ_index, index]
-        last_occ_index = index
-        ################################################################
-        print(sum(sorted_dictionary.values() for i in indices))
+        Q.append(q_value_generator(before_occ_index,index))
+        before_occ_index = index
     index+=1
+#trosku ofajc pri vypocte posledneho prvku
+Q.append(1-sum(P)-sum(Q))
 
+#pomocne vypisy
+# print(P)
+# print(Q)
+# print(len(P))
+# print(len(Q))
+# print(sum(P)+sum(Q))
+file = open('P_Q_values', 'w')
+file.write('P\n')
+file.write(''.join(str(e) for e in P))
+file.write('Q\n')
+file.write(''.join(str(e) for e in Q))
+file.close()
 
 #dlzka mojho pola slov nad 50000
-n = len(over_limit_words_array)
-
-
-
-
-
-
-
-
-
-
-
+n = len(P)
 
 #algorimtus pre vytvorenie OBST zo https://stackoverflow.com/questions/46160969/generating-an-optimal-binary-search-tree-cormen
 
-# p = pd.Series(P, index=range(1, n+1))
-# q = pd.Series(Q)
-#
-# e = pd.DataFrame(np.diag(Q), index=range(1, n+2))
-# w = pd.DataFrame(np.diag(Q), index=range(1, n+2))
-# root = pd.DataFrame(np.zeros((n, n)), index=range(1, n+1), columns=range(1, n+1))
-#
+p = pd.Series(P, index=range(1, n+1))
+q = pd.Series(Q)
+
+e = pd.DataFrame(np.diag(Q), index=range(1, n+2))
+w = pd.DataFrame(np.diag(Q), index=range(1, n+2))
+root = pd.DataFrame(np.zeros((n, n)), index=range(1, n+1), columns=range(1, n+1))
+
 # print(p)
 # print(q)
-# for l in range(1, n+1):
-#     for i in range(1, n-l+2):
-#         j = i+l-1
-#         e.set_value(i, j, np.inf)
-#         w.set_value(i, j, w.get_value(i, j-1) + p[j] + q[j])
-#         for r in range(i, j+1):
-#             t = e.get_value(i, r-1) + e.get_value(r+1, j) + w.get_value(i, j)
-#             if t < e.get_value(i, j):
-#                 e.set_value(i, j, t)
-#                 root.set_value(i, j, r)
-#
-# print(e)
-# print(w)
-# print(root)
+for l in range(1, n+1):
+    for i in range(1, n-l+2):
+        j = i+l-1
+        e.set_value(i, j, np.inf)
+        w.set_value(i, j, w.get_value(i, j-1) + p[j] + q[j])
+        for r in range(i, j+1):
+            t = e.get_value(i, r-1) + e.get_value(r+1, j) + w.get_value(i, j)
+            if t < e.get_value(i, j):
+                e.set_value(i, j, t)
+                root.set_value(i, j, r)
+
+print('e',e)
+print('w',w)
+print('r',root)
+print('---------------------------------------------------')
+print('Priemerny pocet krokov v strome pri vyhladavani prvku v mojom strome = ', e.iat[0,-1])
